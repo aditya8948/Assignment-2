0c7df72ac374a536eb58cfdefaec92b57853498c
adityapandey <89adityapandey@gmail.com>
2026-09-18 16:16:43 +0530

    get from html tag

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get('/api/products', (req, res) => {
+    res.sendFile(path.join(__dirname, 'VIEW', 'product.html'));
+});
+
 app.get('/products', (req, res) => {
     res.sendFile(path.join(__dirname, 'VIEW', 'product.html'));
 });
